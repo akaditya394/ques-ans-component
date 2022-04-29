9cf9620575ad70c9d3c1eb3c328c5eb353db5c8c
Aditya <akaditya394@gmail.com>
2022-04-29 19:47:57 +0530

    no cors

--- src/components/QnA/QnA.jsx
+++ src/components/QnA/QnA.jsx
@@ -11,7 +11,7 @@ function QnA() {
     try {
       const response = await fetch(
         "https://recruitingmonk-v2.azurewebsites.net/qna",
-        { crossDomain:true,mode: "cors" }
+        { crossDomain:true,mode: "no-cors" }
       );
       const data = await response.json();
       setQnas(data);
