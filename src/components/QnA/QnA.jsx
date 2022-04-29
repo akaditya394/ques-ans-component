c17c8fd52f68acbc9df8594370730e2905b25936
Aditya <akaditya394@gmail.com>
2022-04-29 19:27:36 +0530

    mode: "no-cors"

--- src/components/QnA/QnA.jsx
+++ src/components/QnA/QnA.jsx
@@ -11,7 +11,7 @@ function QnA() {
     try {
       const response = await fetch(
         "https://recruitingmonk-v2.azurewebsites.net/qna",
-        { mode: "cors" }
+        { mode: "no-cors" }
       );
       const data = await response.json();
       setQnas(data);
